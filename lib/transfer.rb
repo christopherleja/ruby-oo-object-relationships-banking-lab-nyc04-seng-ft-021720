9564def866fc79a4016d12b539efa14fdf72198c
Christopher Leja <christopher.leja@gmail.com>
2020-02-23 15:48:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   
   def execute_transaction
       
-    if @sender.balance < @amount || @sender.valid? != true
+    if @sender.balance < @amount || @sender.valid? == false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
       
