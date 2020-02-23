48d2278716e308bc7701ce5af2036b809281eaa1
Christopher Leja <christopher.leja@gmail.com>
2020-02-23 15:43:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,12 +17,13 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == "complete"
-      "This transaction has already been completed"
       
-    elsif @sender.balance < @amount
+    if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+      
+    elsif @status == "complete"
+      "This transaction has already been completed"
       
     elsif valid?
       @sender.deposit(@amount * -1) && @receiver.deposit(@amount)
